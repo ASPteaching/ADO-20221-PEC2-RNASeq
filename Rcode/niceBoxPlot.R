523a40308fd413b561f4897173287a07d7e595b4
Jordi Cortes <jordi.cortes-martinez@upc.edu>
2022-10-11 19:11:34 +0200

    Arrange repo

--- Rcode/niceBoxPlot.R
+++ Rcode/niceBoxPlot.R
@@ -50,3 +50,20 @@ niceBoxPlot <- function (aCPMObj){
           legend.box.background = element_rect(fill = "transparent"))
   gg1
 }
+
+niceBoxPlotly <- function (aCPMObj){
+  require(plotly)
+  cols = gg_color_hue(2)
+  d <- tibble(y=as.numeric(aCPMObj),
+              x=rep(colnames(aCPMObj),each=nrow(aCPMObj)),
+              co=factor(rep(ifelse(
+                substr(colnames(aCPMObj),1,3)=='COV',
+                'COVID','HEALTHY'),
+                each=nrow(aCPMObj))))
+  plot_ly(d, x = ~x, y = ~y, type = "box", #color=~cols,
+          colors = gg_color_hue(2)) %>%
+    layout(xaxis = list(title = ''),
+           yaxis = list(title = 'Normalized counts (log2)',
+                        hoverformat = '.2f'),
+           legend = list(x = 0.5, y = 17, orientation = 'h',xanchor = "center"))
+}
